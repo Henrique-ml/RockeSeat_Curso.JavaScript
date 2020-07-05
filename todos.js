74c24ce45cca1b163f69d768d8bde3a247798717
Henrique Lira <henrique.melhado@hotmail.com>
2020-07-05 14:47:28 -0300

    Renderizando Todos (to do)

--- todos.js
+++ todos.js
@@ -1,1 +1,12 @@
-alert('Funcionou!')
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+// Armazenar esses todos no js
+
+var todos =[ // como o TODO são só textos, fazemos simplesmente um array de string
+    //{ text: 'Fazer café, propriedade: 1,}, // caso fosse mais complexo do que um simples texto
+    'Fazer café', 
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
+];
